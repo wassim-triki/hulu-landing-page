4c5f72cde8d48300399d03b580a0186b5f0a599c
Wassim Triki <wsmtriki@gmail.com>
2022-03-05 02:40:34 +0100

    final changes

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -17,6 +19,22 @@ export default function Home() {
       {/* Nav */}
       <Nav />
       {/* Results */}
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  );
+  const data = await request.json();
+  return {
+    props: {
+      results: data.results,
+    },
+  };
 }
